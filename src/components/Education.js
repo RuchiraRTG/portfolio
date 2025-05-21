9b383f59c06aac61f9dc418350d08ddadef5f790
Ruchira tharupathi <ruchiratharupathi01@gmail.com>
2025-05-22 02:18:54 +0530

    Create Education.js

---
+++ src/components/Education.js
@@ -0,0 +1,64 @@
+import { Container, Row, Col } from "react-bootstrap";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+import './Education.css';
+
+export const Education = () => {
+  return (
+    <section className="education" id="education">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Education</h2>
+                  <p>My Academic Journey</p>
+                  <div className="education-container">
+                    <div className="education-timeline">
+                      <div className="education-item university">
+                        <div className="education-content">
+                          <h3>SLIIT</h3>
+                          <h4>BSc (Hons) in Information Technology</h4>
+                          <p className="education-date">2021 - Present</p>
+                          <p className="education-description">
+                            • Currently pursuing a degree in Information Technology<br/>
+                            • Specializing in Software Engineering<br/>
+                            • GPA: 3.8/4.0
+                          </p>
+                        </div>
+                      </div>
+                      <div className="education-item school">
+                        <div className="education-content">
+                          <h3>School Name</h3>
+                          <h4>Advanced Level</h4>
+                          <p className="education-date">2018 - 2020</p>
+                          <p className="education-description">
+                            • Completed Advanced Level in Physical Science Stream<br/>
+                            • Achieved 3A passes<br/>
+                            • Active member of the Science Society
+                          </p>
+                        </div>
+                      </div>
+                      <div className="education-item diploma">
+                        <div className="education-content">
+                          <h3>Diploma in Information Technology</h3>
+                          <h4>Professional Certification</h4>
+                          <p className="education-date">2020 - 2021</p>
+                          <p className="education-description">
+                            • Completed professional certification in IT<br/>
+                            • Focused on web development and programming<br/>
+                            • Achieved distinction in final project
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+} 
